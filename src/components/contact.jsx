8fba99dc1c4637a852a41909537d905895404e83
jeeva <cvasanthkumardpi@gmail.com>
2025-03-07 00:20:06 +0530

    feat : contact us

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -2,7 +2,88 @@
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+      <div className="contactUsBanner">
+        <div className="overlay">
+          <h2>Contact Us</h2>
+        </div>
+      </div>
+
+      <div className="container-sm contactUs py-3">
+       
+
+        <div className="py-2 row align-items-center justify-content-between">
+          <h2 className="text-center py-4">Main Office</h2>
+            <div className="col-12 col-md-7 order-1">
+              <iframe className=" border border-dark border-3" width="100%"  title='googleMap' height="400"  scrolling="no"  src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=The%20Estate%20Building%20,%208th%20Floor,%20Duckenson%20Road%20Bangalore%20,%20Karnataka%20,%20560042%20,%20India+(Cloud%20Arsitek)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+                  {/* <a href="https://www.gps.ie/">gps vehicle tracker</a> */}
+              </iframe>
+            </div>
+            <div className="col-12 col-md-4 py-3 order-md-1">
+                <div className="text-center text-md-start">
+                    <h3 className="py-md-2">Reach Us</h3>
+                    <p>Call or Text : <span className='contact1'>(123) 123-1234</span></p>
+                    <p >Email : <span className='contact1'>goodmanlawfirm@gmail.com</span></p>
+                    <br></br>
+                    <br></br>
+                    <h3 className="py-md-2">Visit Us</h3>
+                    <p>165 Burnside St 2nd Floor Cranston, RI 02910</p>
+                </div>
+            </div>
+           
+        </div>
+
+        <hr className='border border-1 border-danger-subtle my-1'></hr>
+
+        <div className="py-2 row align-items-center justify-content-between">
+            <h2 className="text-center py-4">Branch Office 1</h2>
+            <div className="col-12 col-md-7 order-1">
+              <iframe className=" border border-dark border-3" width="100%"  title='googleMap' height="400"  scrolling="no"  src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=The%20Estate%20Building%20,%208th%20Floor,%20Duckenson%20Road%20Bangalore%20,%20Karnataka%20,%20560042%20,%20India+(Cloud%20Arsitek)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+                  {/* <a href="https://www.gps.ie/">gps vehicle tracker</a> */}
+              </iframe>
+            </div>
+            <div className="col-12 col-md-4 py-3">
+                <div className="text-center text-md-start">
+                    <h3 className="py-md-2">Reach Us</h3>
+                    <p>Call or Text : <span className='contact1'>(123) 123-1234</span></p>
+                    <p >Email : <span className='contact1'>goodmanlawfirm@gmail.com</span></p>
+                    <br></br>
+                    <br></br>
+                    <h3 className="py-md-2">Visit Us</h3>
+                    <p>165 Burnside St 2nd Floor Cranston, RI 02910</p>
+                </div>
+            </div>
+           
+        </div>
+
+        
+        <hr className='border border-1 border-danger-subtle my-1'></hr>
+
+        <div className="py-2 row align-items-center justify-content-between">
+          <h2 className="text-center py-4">Branch Office 2</h2>
+            <div className="col-12 col-md-7 order-1">
+              <iframe className=" border border-dark border-3" width="100%"  title='googleMap' height="400"  scrolling="no"  src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=The%20Estate%20Building%20,%208th%20Floor,%20Duckenson%20Road%20Bangalore%20,%20Karnataka%20,%20560042%20,%20India+(Cloud%20Arsitek)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+                  {/* <a href="https://www.gps.ie/">gps vehicle tracker</a> */}
+              </iframe>
+            </div>
+            <div className="col-12 col-md-4 py-3 order-md-1">
+                <div className="text-center text-md-start">
+                    <h3 className="py-md-2">Reach Us</h3>
+                    <p>Call or Text : <span className='contact1'>(123) 123-1234</span></p>
+                    <p >Email : <span className='contact1'>goodmanlawfirm@gmail.com</span></p>
+                    <br></br>
+                    <br></br>
+                    <h3 className="py-md-2">Visit Us</h3>
+                    <p>165 Burnside St 2nd Floor Cranston, RI 02910</p>
+                </div>
+            </div>
+           
+        </div>
+
+        <hr className='border border-1 border-danger-subtle my-1'></hr>
+
+      </div>
+    </>
   )
 }
 
